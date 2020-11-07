4254b1e968cfafd185b6d70cbbbec3e9df49c274
Axmart <notaxmart@gmail.com>
2020-11-07 22:11:02 +0100

    4. Feladat

--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -15,9 +15,27 @@ namespace Balkezesek
             Jatekosok = Jatekos.Beolvasas();
 
             AdatsorokSzama();
+            JatekosokMagassaga();
 
 
             Console.ReadKey();
+
+        }
+
+        private static void JatekosokMagassaga()
+        {
+
+            Console.WriteLine("4. Feladat:");
+            var output = new Dictionary<string, float>();
+            foreach (var j in Jatekosok)
+            {
+                if (j.UtolsoPalyaraLepes.Year ==  1999 && j.UtolsoPalyaraLepes.Month == 10  )
+                {
+                    Console.WriteLine($"\t{j.Nev}, {(float)j.MagassagInchben*2.54 :#.0} cm");
+                }
+            }
+
+
 
         }
 
